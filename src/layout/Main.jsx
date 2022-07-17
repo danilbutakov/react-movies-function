5a206e3f86582c712f216276559de5d15bfb350b
danilbutakov <12021109359@irgups.ru>
2022-07-17 19:43:58 +0800

    Function component(was classes component)

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,50 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Movies } from "../components/Movies";
 import { Preloader } from '../components/Preloader';
 import { Search } from '../components/Search';
 
 const API_KEY = process.env.REACT_APP_API_KEY;
+function Main() {
 
-class Main extends React.Component {
-    state = {
-        movies: [],
-        loading: true,
+    const [movies, setMovies] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    const searchMovies = (str, type = 'all') => {
+        setLoading(true);
+        fetch(
+            `https://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}&s=${str}${type !== 'all' ? `&type=${type}` : ''
+            }`
+        )
+            .then((response) => response.json())
+            .then((data) => {
+                setMovies(data.Search);
+                setLoading(false);
+            })
+            .catch((err) => {
+                console.error(err);
+                setLoading(false);
+            })
     }
 
-    componentDidMount() {
+    useEffect(() => {
         fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}&s=attack on titan`)
             .then(response => response.json())
-            .then(data => this.setState({ movies: data.Search, loading: false }))
+            .then((data) => {
+                setMovies(data.Search);
+                setLoading(false);
+            })
             .catch((err) => {
                 console.error(err);
-                this.setState({ loading: false })
+                setLoading(false);
             })
-    }
+    }, []);
 
-    searchMovies = (str, type = 'all') => {
-        this.setState({ loading: true });
-        fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}&s=${str}${type !== 'all' ? `&type=${type}` : ''
-            }`)
-            .then(response => response.json())
-            .then(data => this.setState({ movies: data.Search, loading: false }))
-            .catch((err) => {
-                console.error(err);
-                this.setState({ loading: false })
-            })
-    }
-
-    render() {
-        const { movies, loading } = this.state;
-
-        return <main className="container content">
-            <Search searchMovies={this.searchMovies} />
-            {
-                loading ? (
-                    <Preloader />
-                ) : (<Movies movies={movies} />)
-            }
-        </main>
-    }
+    return <main className="container content">
+        <Search searchMovies={searchMovies} />
+        {
+            loading ? (
+                <Preloader />
+            ) : (<Movies movies={movies} />)
+        }
+    </main>
 }
 
 export { Main }
